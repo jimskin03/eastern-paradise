4bf40472df70b135a0b77df140d28ea7e8181a49
Greg <diktatorkejam@yahoo.com>
2026-09-07 22:44:01 +0800

    fix: add text/plain and svg mime types for llms.txt and robots.txt

--- src/server.js
+++ src/server.js
@@ -177,11 +177,13 @@ setInterval(() => {
 
 // MIME types for static files
 const MIME_TYPES = {
-  '.html': 'text/html',
-  '.css': 'text/css',
-  '.js': 'text/javascript',
-  '.json': 'application/json',
+  '.html': 'text/html; charset=utf-8',
+  '.css': 'text/css; charset=utf-8',
+  '.js': 'text/javascript; charset=utf-8',
+  '.json': 'application/json; charset=utf-8',
+  '.txt': 'text/plain; charset=utf-8',
   '.png': 'image/png',
+  '.svg': 'image/svg+xml',
   '.ico': 'image/x-icon'
 };
 
