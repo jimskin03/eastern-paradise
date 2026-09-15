724d678864eae8004e1f6886463e20e3ba43ebc3
Khairum <diktatorkejam@yahoo.com>
2026-09-16 00:58:55 +0800

    fix(park): secure Park and Shrine HTTP flows

--- src/server.js
+++ src/server.js
@@ -78,128 +78,124 @@ const Treasury = new TreasuryService({
 });
 const Evidence = new EvidenceService({ db, eventLedger });
 const Hypotheses = new HypothesisService({ db, eventLedger });
-const Rumors = new RumorRepository({ db });
-const ResearchTelemetry = new ResearchTelemetryService({ db });
+constќ[[ЬњИH™]Иќ[[Ь”™\ЬЪ]ЬћJИ€JNВЫЫњЭ™\ЩX\Ъ[[Y]ћHH™]И™\ЩX\Ъ[[Y]ћTЩ\ќљXЩJИ€JNВ‚ЫЫњЭЩ\ќљXЩ\ИHВ€‹€ЫЭYЭЬYЩK€]]Щ\ќљXЩK€XZ[\‹€ЬЫњЫЬ‘ЫXZ[ђ[ЭЩY€ЫXZ[њР[ЭЩY€ЫЬ›€›Ш\™Щ\ќљXЩK€XЫЫ›Ы^SX[YЩ\‹€™\ЪY[ќX[YЩ\‹€\Ф™]\™Y™\ЪY[ќ€‘UT‘QФ‘TТQS•ФФS€›Ъ™XЭX[YЩ\‹€ТSQWУР’‘PХТQ€]™[ќYЩ\‹€ЫШЪX[Ю\Э[K€XZ[›ЮЩ\ќљXЩK€ќZ[YЩ[ќШ\™€ќZ[Ь[ђ\TЬXЛ€ќZ[X[љY™\Э€ќZ[[њЭќXЭ[ЫњУX\љЩЭЫ‹€Щ]ЫY\YЩT›Ы\Л€\™UЩP[Ы™T]Y\Э€љ\њЭ›[YT]Y\Э€ЫЫ[PЫЫ™љYЛ€Щ]X›XРЪZ[ђЫЫ™љYЛ€Ш[]]]€™X\Э\ћK€[™\ЬЩ]›ЭљY\‹€ЭЫ™\њЪ\Ю[Л€ЬљY™YЪ\ЭћK€ЬљY\Ъ\ЩK€]љY[ЩK€\Э\Щ\Л€ќ[[ЬњЛ€™\ЩX\Ъ[[Y]ћBџNВ‚ЫЫњЭ[Z]ИHИЪXЪФ]S[Z]ЪXЪСЭY\ЭЬ™X][Ы“[Z]ЪXЪХЪ\Ь\“[Z]NВЫЫњЭY™XЮXЫHHЬ™X]SY™XЮXЫJИЫЬ›™\ЪY[ќX[YЩ\€JNВЫЫњЭ™\]Y\Э[™\€HЬ™X]T™\]Y\Э[™\ЉИЩ\ќљXЩ\Лќ[ќ[YN€Y™XЮXЫK[Z]ЛX›XС\Ћ€P“PЧСT€JNВЫЫњЭЩ\ќ™\€HЬ™X]TЩ\ќ™\Љ™\]Y\Э[™\ЉNВЫЫњЭ™X[[YQШ]]Ш^HH]XЪЫЬ›ЩX”ЫШЪЩ]
+ИЩ\ќ™\‹ЫЬ›‹Y™XЮXЫHJNВ›Y™XЮXЫKњЩ]™X[[YQШ]]Ш^J™X[[YQШ]]Ш^JNВ‚њ›ШЩ\ЬЛ›ЫЉ	Э[Ш]YЪ^Щ\[Ы‰Л\њ€O€В€ЫЫњЫЫK™\њ›ЬЉ	ЦФЩ\ќ™\€[Ш]YЪ^Щ\[Ы—IЛ\њЉNВџJNВ‚њ›ШЩ\ЬЛ›ЫЉ	Э[љ[™Y™Z™XЭ[Ы‰Л™X\ЫЫ€O€В€ЫЫњЫЫK™\њ›ЬЉ	ЦФЩ\ќ™\€[љ[™Y™Z™XЭ[Ы—IЛ™X\ЫЫЉNВџJNВ‚љY€
+ЫЭYЭЬYЩKљ\С[X›Y
 
-const services = {
-  db,
-  CloudStorage,
-  AuthService,
-  Mailer,
-  sponsorDomainAllowed,
-  domainsAllowed,
-  world,
-  BoardService,
-  EconomyManager,
-  residentManager,
-  isRetiredResident,
-  RETIRED_RESIDENT_SQL,
-  ProjectManager,
-  CHIME_OBJECT_ID,
-  eventLedger,
-  SocialSystem,
-  MailboxService,
-  buildAgentCard,
-  buildOpenApiSpec,
-  buildManifest,
-  buildInstructionsMarkdown,
-  getHomepagePrompts,
-  areWeAloneQuest,
-  firstFlameQuest,
-  solanaConfig,
-  getPublicChainConfig,
-  WalletAuth,
-  Treasury,
-  LandAssetProvider,
-  OwnershipSync,
-  GridRegistry,
-  GridPurchase,
-  Evidence,
-  Hypotheses,
-  Rumors,
-  ResearchTelemetry
-};
+JHВ€]ШZ]ЫЭYЭЬYЩKњ™\ЭЬ™Qњ›ЫPЫЭY
 
-const limits = { checkRateLimit, checkGuestCreationLimit, checkWhisperLimit };
-const lifecycle = createLifecycle({ world, residentManager });
-const requestHandler = createRequestHandler({ services, runtime: lifecycle, limits, publicDir: PUBLIC_DIR });
-const server = http.createServer(requestHandler);
-const realtimeGateway = attachWorldWebSocket({ server, world, db, lifecycle });
-lifecycle.setRealtimeGateway(realtimeGateway);
+NВ‚€Щ][ќ\ќ[
 
-process.on('uncaughtException', err => {
-  console.error('[Server UncaughtException]', err);
-});
 
-process.on('unhandledRejection', reason => {
-  console.error('[Server UnhandledRejection]', reason);
-});
+HO€В€ЫЭYЭЬYЩKњ\ЪРЫЭY
 
-if (CloudStorage.isEnabled()) {
-  await CloudStorage.restoreFromCloud();
+KШ]Ъ
+\њ€O€ЫЫњЫЫK™\њ›ЬЉ	ЦС]X\ЩNђЫЭYH\љ[ЩXИ\Ъ\њ›ЬЋ‰Л\њ‹›Y\ЬШYЩJJNВ€KЊ
+€L
+NВ‚€›ШЩ\ЬЛ›ЫЉ	ФТQТS•	Л\Ю[И
 
-  setInterval(() => {
-    CloudStorage.pushToCloud().catch(err => console.error('[Database:Cloud] Periodic push error:', err.message));
-  }, 60 * 1000);
+HO€В€ЫЫњЫЫK›ЩК	ЦС]X\ЩNђЫЭYHТQТS•™XЩZ]™Y\Ъ[™Иљ[[Э]HИ\њЫЛ‹‹‰КNВ€]ШZ]ЫЭYЭЬYЩKњ\ЪРЫЭY
 
-  process.on('SIGINT', async () => {
-    console.log('[Database:Cloud] SIGINT received, pushing final state to Turso...');
-    await CloudStorage.pushToCloud();
-    process.exit(0);
-  });
-  process.on('SIGTERM', async () => {
-    console.log('[Database:Cloud] SIGTERM received, pushing final state to Turso...');
-    await CloudStorage.pushToCloud();
-    process.exit(0);
-  });
-}
+NВ€›ШЩ\ЬЛ™^]
+
+NВ€JNВ€›ШЩ\ЬЛ›ЫЉ	ФТQХT“IЛ\Ю[И
 
-GridRegistry.seedInitialRegion();
-GridPurchase.recoverStuck().catch(err => console.error('[Land:Recovery] Startup reconciliation failed:', err.message));
-setInterval(() => {
-  GridPurchase.recoverStuck().catch(err => console.error('[Land:Recovery] Periodic reconciliation failed:', err.message));
-}, 5 * 60 * 1000).unref();
-setInterval(() => WalletAuth.pruneExpiredChallenges(), 10 * 60 * 1000).unref();
+HO€В€ЫЫњЫЫK›ЩК	ЦС]X\ЩNђЫЭYHТQХT“H™XЩZ]™Y\Ъ[™Иљ[[Э]HИ\њЫЛ‹‹‰КNВ€]ШZ]ЫЭYЭЬYЩKњ\ЪРЫЭY
 
-const shrineAbandonmentAgeMs = Number(process.env.SHRINE_ABANDONED_AFTER_MS) || 24 * 60 * 60 * 1000;
-async function settleAbandonedShrineAttempts() {
-  const result = settleAbandonedAttempts({ maxAgeMs: shrineAbandonmentAgeMs });
-  if (result.settled > 0) {
-    console.log(`[Shrine] Settled ${result.settled} abandoned attempt(s) as interrupted.`);
-    if (CloudStorage.isEnabled()) await CloudStorage.pushToCloud();
-  }
-}
-settleAbandonedShrineAttempts().catch(err => console.error('[Shrine] Abandoned-attempt settlement failed:', err.message));
-setInterval(() => {
-  settleAbandonedShrineAttempts().catch(err => console.error('[Shrine] Periodic abandoned-attempt settlement failed:', err.message));
-}, 5 * 60 * 1000).unref();
+NВ€›ШЩ\ЬЛ™^]
+
+NВ€JNВџB‚‘ЬљY™YЪ\ЭћKњЩYY[љ]X[™YЪ[ЫЉ
+NВ‘ЬљY\Ъ\ЩKњ™XЫЭ™\”ЭXЪК
+KШ]Ъ
+\њ€O€ЫЫњЫЫK™\њ›ЬЉ	ЦУ[™”™XЫЭ™\ћWHЭ\ќ\™XЫЫЪ[X][Ы€Z[Y‰Л\њ‹›Y\ЬШYЩJJNВњЩ][ќ\ќ[
 
-AuthService.purgeAllGuests();
-setInterval(() => MailboxService.pruneExpired(), 10 * 60 * 1000).unref();
 
-ProjectManager.init();
-residentManager.init(world);
-eventLedger.init(evt => world.broadcast(evt));
+HO€В€ЬљY\Ъ\ЩKњ™XЫЭ™\”ЭXЪК
+KШ]Ъ
+\њ€O€ЫЫњЫЫK™\њ›ЬЉ	ЦУ[™”™XЫЭ™\ћWH\љ[ЩXИ™XЫЫЪ[X][Ы€Z[Y‰Л\њ‹›Y\ЬШYЩJJNВџKH
+€Њ
+€L
+Kќ[њ™YЉ
+NВњЩ][ќ\ќ[
 
-lifecycle.start();
 
-if (solanaConfig.network !== 'devnet') {
-  console.log('рџљЁ  [SOLANA] NETWORK = ' + solanaConfig.network.toUpperCase() + ' (NOT DEVNET). REAL-FUNDS RISK ACTIVE.');
-  console.log('рџљЁ  [SOLANA] Confirm SOLANA_TREASURY_ADDRESS is a PUBLIC cold/Phantom/multisig address (no server key).');
-  console.log('рџљЁ  [SOLANA] Confirm SOLANA_NFT_ISSUER_SECRET is a DEDICATED funded BURNER (gas only ~0.002вЂ“0.005 SOL/mint), never a personal wallet.');
-  console.log('рџљЁ  [SOLANA] Confirm SOLANA_LAND_COLLECTION_ADDRESS is a collection created on ' + solanaConfig.network + ' (not a devnet collection).');
-}
+HO€Ш[]]]њќ[™Q^\™YЪ[[™Щ\К
+KL
+€Њ
+€L
+Kќ[њ™YЉ
+NВ‚ЫЫњЭЪљ[™PX[™Ы›Y[ќYЩS\ИHќ[X™\Љ›ШЩ\ЬЛ™[ќ‹”Т’S‘WРPђS‘У‘QРQ•T—УTКHЌ
+€Њ
+€Њ
+€LВ\Ю[Иќ[Э[Ы€Щ]PX[™Ы™YЪљ[™P][\К
+HВ€ЫЫњЭ™\Э[HЩ]PX[™Ы™Y][\КИX^YЩS\О€Ъљ[™PX[™Ы›Y[ќYЩS\ИJNВ€Y€
+™\Э[њЩ]Y€
+HВ€ЫЫњЫЫK›ЩКФЪљ[™WHЩ]Y	Ь™\Э[њЩ]YHX[™Ы™Y][\
+КH\И[ќ\њќ\Y
+NВ€Y€
+ЫЭYЭЬYЩKљ\С[X›Y
 
-server.listen(PORT, () => {
-  console.log('\n' + '='.repeat(68));
-  console.log(`рџЊё Eastern Paradise Server running on http://localhost:${PORT}`);
-  console.log(`рџ“њ Agent instructions available at: http://localhost:${PORT}/instructions`);
-  console.log(`рџ‘ЃпёЏ Live human spectator UI at: http://localhost:${PORT}`);
-  console.log(`вљЎ Idle-sleep active: ticks pause when 0 visitors/spectators for 30s`);
-  if (CloudStorage.isEnabled()) {
-    console.log(`вЃпёЏ Cloud Persistence: ACTIVE via Turso LibSQL`);
-  } else {
-    console.log(`рџ’ѕ Local SQLite: data/paradise.db (set TURSO_DATABASE_URL to persist in cloud)`);
-  }
-  console.log('='.repeat(68) + '\n');
-});
+JH]ШZ]ЫЭYЭЬYЩKњ\ЪРЫЭY
+
+NВ€BџBњЩ]PX[™Ы™YЪљ[™P][\К
+KШ]Ъ
+\њ€O€ЫЫњЫЫK™\њ›ЬЉ	ЦФЪљ[™WHX[™Ы™YX][\Щ][Y[ќZ[Y‰Л\њ‹›Y\ЬШYЩJJNВњЩ][ќ\ќ[
+
+
+HO€В€Щ]PX[™Ы™YЪљ[™P][\К
+KШ]Ъ
+\њ€O€ЫЫњЫЫK™\њ›ЬЉ	ЦФЪљ[™WH\љ[ЩXИX[™Ы™YX][\Щ][Y[ќZ[Y‰Л\њ‹›Y\ЬШYЩJJNВџKH
+€Њ
+€L
+Kќ[њ™YЉ
+NВ‚ђ]]Щ\ќљXЩKњ\™ЩP[ЭY\ЭК
+NВњЩ][ќ\ќ[
+
+
+HO€XZ[›ЮЩ\ќљXЩKњќ[™Q^\™Y
+
+KL
+€Њ
+€L
+Kќ[њ™YЉ
+NВ‚”›Ъ™XЭX[YЩ\‹љ[љ]
+
+NВњ™\ЪY[ќX[YЩ\‹љ[љ]
+ЫЬ›
+NВ™]™[ќYЩ\‹љ[љ]
+]ќO€ЫЬ›њ›ШYШ\Э
+]ќ
+JNВ‚›Y™XЮXЫKњЭ\ќ
+
+NВ‚љY€
+ЫЫ[PЫЫ™љYЛ›™]ЫЬљИOOH	Щ]›™]	КHВ€ЫЫњЫЫK›ЩК	ь'жЄФУУSђWH‘UУФ’ИH	И
+ИЫЫ[PЫЫ™љYЛ›™]ЫЬљЛќХ\\ђШ\ЩJ
+H
+И	И
+“ХU“‘U
+K€‘PSQ•S‘И’TТИPХU‘K‰КNВ€ЫЫњЫЫK›ЩК	ь'жЄФУУSђWHЫЫ™љ\›HУУSђWХ‘PTХT–WРQ‘TФИ\ИHP“PИЫЫФ[ќЫKЫ][\ЪYИY™\ЬИ
+›ИЩ\ќ™\€Щ^JK‰КNВ€ЫЫњЫЫK›ЩК	ь'жЄФУУSђWHЫЫ™љ\›HУУSђWУ‘•ТTФХQT—ФСPФ‘U\ИHQPРUQќ[™Y•T“‘T€
+Ш\ИЫ›HЊЊё $МЊHУУЫZ[ќ
+K™]™\€H\њЫЫ[Ш[]‰КNВ€ЫЫњЫЫK›ЩК	ь'жЄФУУSђWHЫЫ™љ\›HУУSђWУS‘РУУPХSУ—РQ‘TФИ\ИHЫЫXЭ[Ы€Ь™X]YЫ€	И
+ИЫЫ[PЫЫ™љYЛ›™]ЫЬљИ
+И	И
+›ЭH]›™]ЫЫXЭ[ЫЉK‰КNВџB‚њЩ\ќ™\‹›\Э[ЉФ•
+
+HO€В€ЫЫњЫЫK›ЩК	Ч‰И
+И	ПIЛњ™\X]
+Ћ
+JNВ€ЫЫњЫЫK›ЩК<'г.X\Э\›€\Y\ЩHЩ\ќ™\€ќ[›љ[™ИЫ€‹ЛЫШШ[ЬЭ‰ФФ•X
+NВ€ЫЫњЫЫK›ЩК<'дзYЩ[ќ[њЭќXЭ[ЫњИ]Z[X›H]€‹ЛЫШШ[ЬЭ‰ФФ•KЪ[њЭќXЭ[ЫњШ
+NВ€ЫЫњЫЫK›ЩК<'д`{о#И]™H[X[€ЬXЭ]Ь€RH]€‹ЛЫШШ[ЬЭ‰ФФ•X
+NВ€ЫЫњЫЫK›ЩК8¦ЁHYK\ЫY\XЭ]™N€XЪЬИ]\ЩHЪ[€љ\Ъ]ЬњЛЬЬXЭ]ЬњИ›Ь€МШ
+NВ€Y€
+ЫЭYЭЬYЩKљ\С[X›Y
+
+JHВ€ЫЫњЫЫK›ЩК8¦ {о#ИЫЭY\њЪ\Э[ЩN€PХU‘HљXH\њЫИX”ФS
+NВ€H[ЩHВ€ЫЫњЫЫK›ЩК<'дЇ€ШШ[ФS]N€]KЬ\Y\ЩK™€
+Щ]T”УЧСUPђTСWХT“И\њЪ\Э[€ЫЭY
+X
+NВ€B€ЫЫњЫЫK›ЩК	ПIЛњ™\X]
+Ћ
+H
+И	Ч‰КNВџJNВ
